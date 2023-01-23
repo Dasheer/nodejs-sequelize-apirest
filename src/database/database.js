c248eb728a6dda7ff5d8fd12140ef52b39180dcb
Daniel <danieleduardo.pc@outlook.com>
2023-01-22 20:41:11 -0600

    Sequelize Schemas

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize'
 
-export const sequelize = new Sequelize('sequelize_projectdb', 'postgres', '$DPC2104?', {
+export const sequelize = new Sequelize('postgres_projectdb', 'postgres', '$DPC2104?', {
     host: 'localhost',
     dialect: 'postgres'
 })
